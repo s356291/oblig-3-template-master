2a3e45eebd6212b545a3c643a21e30b655153b43
s356291 <89486176+s356291@users.noreply.github.com>
2022-10-26 13:18:54 +0200

    Startet på oppgave 3

--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -135,7 +135,17 @@ public class SBinTre<T> {
     }
 
     private static <T> Node<T> førstePostorden(Node<T> p) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        Objects.requireNonNull(p);      //Sjekker nullverdi
+
+        while (true) {
+            if (p.venstre != null){
+                p = p.venstre;
+            }else if(p.høyre != null){
+                p = p.høyre;
+            }else{
+                return p;
+            }
+        }
     }
 
     private static <T> Node<T> nestePostorden(Node<T> p) {
